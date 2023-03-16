97d029fa37fd1b9b9c02d1d17d7805bb6438b86a
Taijaun Pitt <taijaun@hotmail.co.uk>
2023-03-16 07:41:23 +0000

    - Created initial UI in the storyboard - added IB Outlet for tableview - made the view controller class conform to the tableview delegate and datasource protocols

---
+++ Journal App/NotesModel.swift
@@ -0,0 +1,14 @@
+//
+//  NotesModel.swift
+//  Journal App
+//
+//  Created by Taijaun Pitt on 16/03/2023.
+//
+
+import Foundation
+
+class NotesModel {
+    
+    
+    
+}
--- Journal App/ViewController.swift
+++ Journal App/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var notes = [Note]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
+    
+    
 }
 
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return notes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NoteCell", for: indexPath)
+        
+        // TODO: Customise cell
+        
+        return cell
+        
+    }
+    
+    
+    
+    
+}
