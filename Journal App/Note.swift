c7e32e3628203a561d2fd27d5a5c4483f3bc3dc9
Taijaun Pitt <taijaun@hotmail.co.uk>
2023-03-16 07:22:32 +0000

    initial commit

---
+++ Journal App/Note.swift
@@ -0,0 +1,19 @@
+//
+//  Note.swift
+//  Journal App
+//
+//  Created by Taijaun Pitt on 16/03/2023.
+//
+
+import Foundation
+
+struct Note {
+    
+    var docId: String
+    var title: String
+    var body: String
+    var isStarred: Bool
+    var createdAt: Date
+    var lastUpdatedAt: Date
+    
+}
